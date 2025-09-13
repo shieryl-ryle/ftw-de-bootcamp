7c9cd92162c3eaa8504a50ea6673baac2011f84b
myk <myk.ogbinar@yahoo.com.ph>
2025-09-13 12:13:44 +0800

    update

--- dbt/transforms/01_mpg/target/compiled/ex_01_mpg/models/clean/mpg_standardized.sql
+++ dbt/transforms/01_mpg/target/compiled/ex_01_mpg/models/clean/mpg_standardized.sql
@@ -4,15 +4,15 @@
 -- Keep them as-is to preserve nullability and avoid insert errors.
 
 select
-  toFloat64OrNull(mpg)          as mpg,
-  toInt32OrNull(cylinders)      as cylinders,
-  toFloat64OrNull(displacement) as displacement,
+  toFloat64(mpg)          as mpg,
+  toInt32(cylinders)      as cylinders,
+  toFloat64(displacement) as displacement,
   toFloat64OrNull(horsepower)   as horsepower,
-  toInt32OrNull(weight)         as weight,
-  toFloat64OrNull(acceleration) as acceleration,
-  toInt32OrNull(model_year)     as model_year,
+  toInt32(weight)         as weight,
+  toFloat64(acceleration) as acceleration,
+  toInt32(model_year)     as model_year,
   trim(origin)                  as origin,
   trim(name)                    as make
-from `raw`.`autompg___cars`
+from `raw`.`autompg___cars_myk`
 where horsepower is not null
   and isFinite(horsepower)
